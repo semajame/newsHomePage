79f687dd25417ba869a234a5c1fd931752daa503
James <119842373+semajame@users.noreply.github.com>
2023-01-27 15:29:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    menu = document.querySelector(".menu");
+
+    menu.onclick = function(){
+        navmenu = document.querySelector(".nav_menu");
+
+        navmenu.classList.toggle("active");
+   }
